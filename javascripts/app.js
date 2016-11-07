8f85ea864dff5d6c686762f15c580d6f3a59effb
Dani Adkins <danirenaeadkins12@gmail.com>
2016-11-06 21:56:50 -0600

    added additional information to app.js

--- javascripts/app.js
+++ javascripts/app.js
@@ -7,6 +7,7 @@ console.log("robot");
 $(document).ready(function(){
   var robotBrawl;
 
+$("#class-select").hide();
 
   $("#player-setup").show();
   var player1;  // what are we defining here? why define variable?
@@ -23,35 +24,66 @@ $(document).ready(function(){
 
 
 
-$(".")/*class name here*/.on("click", function(event){
+$(".card__link").on("click", function(event){
   var nextCard = $(this).attr("next");
+  var moveAlong = false;
   //
 
 switch (nextCard) {
-  case "" /*class name here */ :
+  case "card--class" :
     moveAlong = ($("player1-name").val() !== "");
-    var playerOne = $("player1-name").val();
-    var playerTwo = $("player2-name").val();
-    player1 = new Robot.OldRobot.Warriors(); //check if this is correct
-    player2 = new Robot.OldRobot.Warriors(); //check if this is correct
-    player1.playerName = playerOne;
-    player2.playerName = playerTwo;
-  break;
-}
-  case "" /*class name for robot selection here*/
+    moveAlong += ($("player2-name").val() !== "");
+    console.log( "card class");
+    var playerOne = $("#player1-name").val();
+    var playerTwo = $("#player2-name").val();
+console.log(playerOne);
+console.log(playerTwo);
+
+    // // player1 = new Robot.OldRobot.Warriors(); //check if this is correct
+    // // player2 = new Robot.OldRobot.Warriors(); //check if this is correct
+    // player1.playerName = playerOne;
+    // player2.playerName = playerTwo;
+
+break;
+
+  case "" :/*class name for robot selection here*/
     moveAlong = (player1.robot !== null);
       if(player1.robot === null) {
         alert ("Please choose a robot.");
-        break;
+      }
+     moveAlong += (player2.robot !== null);
+      if(player2.robot === null) {
+        alert ("Please choose a robot!")
       }
       break;
+
       default :
       console.log("everything is broken fml!")
-
+}
       if (moveAlong) {
         $(".card").hide();
         $("." + nextCard).show(); //understand this better
       }
+    });
+
+$("#player1Robot").on("click", function(event){
+  var test = $(event.target).is("btn-text");
+    if(test){
+      let clicked = $(event.target).find("btn-text".)context.innerHTML;
+      console.log("clicked", clicked);
+      player1.robot = new Robot.OldRobot[clicked]();
+      console.log("player 1", player1);
+    }
+    event.preventDefault();
+});
+
+
+//card back function - back button needs to be installed in each page - if want to use
+// $(".card__back").click(function(e) {
+//     var previousCard = $(this).attr("previous");
+//     $(".card").hide();
+//     $("." + previousCard).show();
+//   });
 });
 
 
